c71f8a42c889f0429046f384d084a6d74864d56c
Christopher <chbeine@gmx.de>
2019-05-18 02:23:47 +0200

    unified model for csv and xes import defined

--- refiningEventLabels/eventLogConverter/concreteImplementation.py
+++ refiningEventLabels/eventLogConverter/concreteImplementation.py
@@ -2,6 +2,8 @@ from .fileUtility import FileUtilityBase
 from .fileHandlerBase import FileConverter
 from pm4py.objects.log.importer.xes import factory as xes_importer
 from pm4py.objects.log.importer.csv import factory as csv_importer
+from pm4py.objects.conversion.log import factory as conversion_factory
+from pm4py.objects.log.util import sorting
 
 class FileUtility(FileUtilityBase):
 
@@ -15,12 +17,13 @@ class XESFileConverter(FileConverter):
         self._fileType = '.xes'
 
     def convert(self, filePath):
-        return xes_importer.import_log(filePath)
+        return xes_importer.apply(filePath)
 
 class CSVFileConverter(FileConverter):
     def __init__(self):
         self._fileType = '.csv'
 
     def convert(self, filePath):
-        return csv_importer.import_event_log(filePath)
+        csvEventStream = csv_importer.import_event_stream(filePath)
+        return conversion_factory.apply(csvEventStream)
         
