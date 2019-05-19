af59721f72caf26ebcbd7a9961cc35fecbfc348d
NicoleVentsch <nicole.ventsch@rwth-aachen.de>
2019-05-19 10:21:00 +0200

    preprocessing added

---
+++ refiningEventLabels/PreprocessingLog/preprocessing.py
@@ -0,0 +1,20 @@
+import pm4py
+from pm4py.objects.log.importer.xes import factory as xes_importer
+
+
+
+# get the variants with the corresponding IDs (-> lookup-map)
+# input: log (imported with xes_importer)
+# stored as a dicctionary with variants as key and as value the list of case IDs that share the variant
+from pm4py.algo.filtering.tracelog.variants import variants_filter
+
+def lookUpTable(log):
+    variant=variants_filter.get_variants_from_log_trace_idx(log, parameters=None)
+    return variant
+
+# get the variants from the dicctionary created with lookUpTable() and omit the IDs
+# input: dicctionary with variants as keys and case IDs as values
+# stored as a list containing the variants as lists (-> list of lists)
+def getVariants(dicctionary):
+    variants=list(dicctionary.keys())
+    return [variant.split(',') for variant in variants]
--- refiningEventLabels/__init__.py
+++ refiningEventLabels/__init__.py
@@ -1,1 +1,2 @@
 from refiningEventLabels import eventLogConverter
+from refiningEventLabels import PreprocessingLog
