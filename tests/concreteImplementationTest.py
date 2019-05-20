2f3c4db41a3ff695dd849f9b9a9ac73078081d2b
Christopher <chbeine@gmx.de>
2019-05-21 01:26:27 +0200

    File export unit test added
    - unit test can be expand for multiple files types

--- tests/concreteImplementationTest.py
+++ tests/concreteImplementationTest.py
@@ -1,8 +1,10 @@
 import unittest
 import os
+import glob
 from refiningEventLabels.eventLogConverter import FileConverter
 from refiningEventLabels.eventLogConverter import FileConverterFactory, FileCreatorFactory
 from refiningEventLabels.eventLogConverter import FileUtility
+from refiningEventLabels.eventLogConverter import XESFileCreator
 from pm4py.objects.log.util import sampling, sorting
 
 class TestFileConverterFactory(unittest.TestCase):
@@ -30,7 +32,7 @@ class FileUtilityTest(unittest.TestCase):
     def testXESConversion(self):
         dirname = os.path.dirname(__file__)
         rootDir = os.path.join(dirname, 'TestFiles')
-        xesPath = os.path.join(dirname, 'Ressources/running-example.xes')
+        xesPath = os.path.join(dirname, 'Ressources/example.xes')
 
         fileCreator = FileUtility(rootDir)
         eventLog = fileCreator.getEventLogFromFile(xesPath)
@@ -40,12 +42,51 @@ class FileUtilityTest(unittest.TestCase):
     def testCSVConversion(self):
         dirname = os.path.dirname(__file__)
         rootDir = os.path.join(dirname, 'TestFiles')
-        csvPath = os.path.join(dirname, 'Ressources/running-example.csv')
+        csvPath = os.path.join(dirname, 'Ressources/example.csv')
 
         fileCreator = FileUtility(rootDir)
         eventLog = fileCreator.getEventLogFromFile(csvPath)
         eventLog = sorting.sort_timestamp(eventLog)
         self.assertEqual(len(eventLog), 6)
+
+# Tests if FileCreators can create a file with the correct path and extension. 
+# The file structure is not checked, because it is set by the creator
+# To test multiple FileCreator register the creator with the registerFileCreator function
+class FileCreatorTest(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(FileCreatorTest, self).__init__(*args, **kwargs)
+        self._fileCreator = set()
+        self.registerFileCreator('xes', XESFileCreator())
+
+    def registerFileCreator(self, type, instance):
+        self._fileCreator.add((type, instance))
+
+    def getFileCreators(self):
+        return self._fileCreator
+
+    def testFileCreator(self):
+        fileCreators = self.getFileCreators()
+        dirname = os.path.dirname(__file__)
+        xesPath = os.path.join(dirname, 'Ressources/example.xes')
+        destinationPath = os.path.join(dirname, 'TestFiles' )
+        fileCreator = FileUtility(xesPath)
+        eventLog = fileCreator.getEventLogFromFile(xesPath)
+        for creatorInfo in fileCreators: 
+            creatorInfo[1].createFile(eventLog, "testFile." + creatorInfo[0], destinationPath)
+        
+        for creatorInfo in fileCreators:
+            shouldCreatedFile = os.path.join(destinationPath, "testFile." + creatorInfo[0])
+            self.assertTrue(os.path.isfile(shouldCreatedFile))
+        self._deletingTestFiles()
+        
+        
+    def _deletingTestFiles(self):
+        dirname = os.path.dirname(__file__)
+        destinationPath = os.path.join(dirname, 'TestFiles')
+        files = glob.glob(os.path.join(destinationPath, '*'))
+        for f in files:
+            os.remove(f)
+                
 
         
 class DummyXesConverter(FileConverter):
