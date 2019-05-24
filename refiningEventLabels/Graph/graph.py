f896bcfca81fd6a4ab3d81e6e45ba38aa3e6c367
Bianka Bakullari <biankabakullari@gmail.com>
2019-05-24 11:07:28 +0200

    graph nodes added

---
+++ refiningEventLabels/Graph/graph.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 24 11:04:20 2019
+
+@author: Bianka
+"""
+
+import mappings
+import cost
+import networkx as nx
+
+#returns a Graph where the vertex correspond to all unique pairs (Id, event label) appearing in the variants
+def createGraph(variants):
+    G = nx.Graph()
+    for variant in variants:
+        for pair in variant:
+            G.add_node(pair)
+    return G
