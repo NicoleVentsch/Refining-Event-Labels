7e6a60e17a0bb9c018b293fa33fed29975c64853
Juan Jose Garza <juanjo_221093@outlook.com>
2019-06-03 09:46:33 +0200

    added get/set for candidate labels in cutomParams object

--- refiningEventLabels/lib/objects/customParameters.py
+++ refiningEventLabels/lib/objects/customParameters.py
@@ -1,13 +1,16 @@
 
 class customParameters:
     
-    def __init__(self, horizontalTreshold, verticalTreshold, weightStructure, weightMatch, weightNoMatch):
+    def __init__(self, candidateLabels, horizontalTreshold, verticalTreshold, weightStructure, weightMatch, weightNoMatch):
+        self.__candidateLabels = candidateLabels
         self.__horizontalTreshold = horizontalTreshold
         self.__verticalTreshold = verticalTreshold
         self.__weightStructure = weightStructure
         self.__weightMatch = weightMatch
         self.__weightNoMatch = weightNoMatch
         
+    def getcandidateLabels(self):
+        return self.__candidateLabels
     
     def getHorizontalTreshold(self):
         return self.__horizontalTreshold
@@ -23,6 +26,9 @@ class customParameters:
         
     def getMatchWeight(self):
         return self.__weightMatch
+    
+    def setcandidateLabels(self, candidateLabels):
+        self.__candidateLabels = candidateLabels
     
     def setHorizontalTreshold(self, horizontalTreshold):
         self.__horizontalTreshold = horizontalTreshold
